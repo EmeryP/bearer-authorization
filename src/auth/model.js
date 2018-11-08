2fc1465cb868b77b5696ad3fcd987ad72683158d
emeryparks <emeryparks09@hotmail.com>
2018-11-07 16:04:52 -0800

    model updated with token auth

--- src/auth/model.js
+++ src/auth/model.js
@@ -1,46 +1,43 @@
-'use strict';
-
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
-const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  password: {type: String, required: true},
-  email: {type: String},
-  role: {type: String, required:true, default:'user', enum:['admin','editor','user'] },
+const schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, required: true, default: 'user', enum: ['admin', 'editor', 'user'] },
 });
 
 const capabilities = {
   user: ['read'],
-  editor: ['create', 'read','update'],
+  editor: ['create', 'read', 'update'],
   admin: ['create', 'read', 'update', 'delete'],
 };
 
-userSchema.pre('save', function(next) {
-  bcrypt.hash(this.password,10)
-    .then(hashedPassword => {
-      this.password = hashedPassword;
-      next();
-    })
-    .catch( error => {throw error;} );
+//hash the users password and populate this.password with the hashed password
+schema.pre('save', async function () {
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+  } catch (error) {
+    throw error;
+  }
 });
 
-userSchema.methods.can = function(capability) {
+schema.method.can = function (capability) {
   return capabilities[this.role].includes(capability);
 };
 
-
-userSchema.statics.authenticateBasic = function(auth) {
-  let query = {username:auth.username};
+schema.statics.authenticateBasic = function (auth) {
+  let query = { username: auth.username };
   return this.findOne(query)
     .then(user => user && user.comparePassword(auth.password))
-    .catch(console.error);
+    .catch(error => error);
 };
 
-userSchema.statics.authenticateToken = function(token) {
+schema.statics.authenticateToken = function (token) {
   let parsedToken = jwt.verify(token, process.env.SECRET);
-  let query = {_id:parsedToken.id};
+  let query = { _id: parsedToken.id };
   return this.findOne(query)
     .then(user => {
       return user;
@@ -48,19 +45,18 @@ userSchema.statics.authenticateToken = function(token) {
     .catch(error => error);
 };
 
-// Compare a plain text password against the hashed one we have saved
-userSchema.methods.comparePassword = function(password) {
+schema.methods.generateToken = function () {
+  const tokenData = {
+    id: this._id,
+    capabilities: capabilities[this.role],
+  };
+  return jwt.sign(tokenData, process.env.APP_SECRET);
+};
+
+schema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password)
     .then(valid => valid ? this : null);
 };
 
-// Generate a JWT from the user id and a secret
-userSchema.methods.generateToken = function() {
-  let tokenData = {
-    id:this._id,
-    capabilities: capabilities[this.role],
-  };
-  return jwt.sign(tokenData, process.env.SECRET);
-};
 
-export default mongoose.model('users', userSchema);
+export default mongoose.model('User', schema);
